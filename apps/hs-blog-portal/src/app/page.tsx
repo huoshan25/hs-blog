777cb1d7b8c786080312a32ab1d8d6929ebe7efa
huoshan <1726941245@qq.com>
2025-04-29 18:26:49 +0800

    feat(CI/CD): 工作流拆分调试

--- apps/hs-blog-portal/src/app/page.tsx
+++ apps/hs-blog-portal/src/app/page.tsx
@@ -4,7 +4,6 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useHomePageTranslation } from '@/hooks/useTranslation'
 
-
 export default function Home() {
   const lang = useHomePageTranslation()
   return (
