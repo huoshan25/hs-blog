0199ef711553616d542cc35fb6753eb8abe3432e
huoshan <1726941245@qq.com>
2025-05-04 17:39:13 +0800

    feat(blog): 搜索页暂提

---
+++ apps/hs-blog-web/src/pages/search/components/tagList.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import {getTagsQuery, type TagsList} from "~/api/tag";
+import {HttpStatus} from "~/enums/httpStatus";
+
+const route = useRoute()
+const tagsList = ref<TagsList[]>([])
+
+const loading = ref(false)
+
+const getTagsList = async () => {
+  try {
+    loading.value = true
+    const res = await getTagsQuery({keyword: route.query.keyword as string})
+    if (res.code === HttpStatus.OK) {
+      tagsList.value = res.data
+    }
+  } finally {
+    loading.value = false
+  }
+}
+watch(() => route.query, getTagsList, {deep: true});
+onMounted(() => {
+  if(tagsList.value.length === 0) {
+    getTagsList()
+  }
+})
+</script>
+
+<template>
+  <div class="flex flex-col bg-white w-[100%] min-h-[100vh]">
+    <div class="flex p-[20px] custom-border-bottom-1px_solid_#E4E6EB7F hover:bg-#F7F8FA cursor-pointer" v-for="tag in tagsList" :key="tag.id">
+      <img class="w-[40px] h-[40px]" src="~/assets/images/tag.png" alt="标签图片">
+      <div class="flex flex-1 flex-col ml-[10px]">
+        <div v-html="tag.name"></div>
+        <div>{{ tag.articleCount }}  文章</div>
+      </div>
+    </div>
+
+    <no-more-data-divider :hasMore="loading"/>
+  </div>
+</template>
+
+<style scoped lang="scss">
+:deep(em) {
+  color: red;
+  font-style: normal;
+  margin: 0.2em;
+}
+</style>
---
+++ apps/hs-blog-web/src/pages/search/index.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+  import ArticleList from './components/articleList.vue'
+  import TagList from './components/tagList.vue'
+  import { useTabs } from './components/useTabs'
+  import { SearchDimension } from './components/enum'
+
+  definePageMeta({
+    layout: 'default'
+  })
+
+  const { scrollY } = useScrollWatcher()
+
+  const { currentTab, handleUpdateValue } = useTabs()
+
+  const isNavbarVisible = computed(() => {
+    return scrollY.value === 0
+  })
+</script>
+
+<template>
+  <div class="main">
+    <header class="w-full header-container" :class="{ 'header-hidden': !isNavbarVisible }">
+      <div class="tab-wrapper">
+        <ClientOnly>
+          <n-tabs v-model:value="currentTab" type="line" animated @update:value="handleUpdateValue">
+            <n-tab-pane :name="SearchDimension.SYNTHESIS" tab="综合" />
+            <n-tab-pane :name="SearchDimension.ARTICLE" tab="文章" />
+            <n-tab-pane :name="SearchDimension.TAG" tab="标签" />
+          </n-tabs>
+        </ClientOnly>
+      </div>
+    </header>
+
+    <div class="contents">
+      <!-- 文章列表 -->
+      <article-list v-if="currentTab !== SearchDimension.TAG" />
+      <!-- 标签列表 -->
+      <tag-list v-else />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .main {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+  }
+
+  .header-container {
+    padding: 0 3vw;
+    background-color: white;
+    display: flex;
+    justify-content: center;
+    position: fixed;
+    top: 60px;
+    box-shadow: inset 0 7px 8px -6px rgba(234, 232, 232, 0.9);
+    transition: top 0.2s ease-in-out;
+
+    &.header-hidden {
+      top: 0;
+    }
+  }
+
+  .tab-wrapper {
+    width: 100%;
+    max-width: 900px;
+    padding: 5px;
+  }
+
+  .contents {
+    width: 100%;
+    max-width: 920px;
+    display: flex;
+    background-color: white;
+    flex-direction: column;
+    margin-top: 50px;
+  }
+
+  :deep(.n-tabs .n-tabs-nav.n-tabs-nav--line-type.n-tabs-nav--top .n-tabs-nav-scroll-content) {
+    border-bottom: none;
+  }
+
+  :deep(.n-tab-pane),
+  :deep(.n-tabs-bar) {
+    display: none;
+  }
+
+  @media (max-width: 767px) {
+    .tab-wrapper {
+      padding: 5px 10px;
+    }
+  }
+</style>
