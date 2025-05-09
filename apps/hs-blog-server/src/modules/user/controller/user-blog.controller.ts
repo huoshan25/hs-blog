4b22b2928a397a85a074a32773ce0b62d9083866
huoshan <1726941245@qq.com>
2025-05-09 15:13:56 +0800

    feat(server): 评论接口jwt校验调整

--- apps/hs-blog-server/src/modules/user/controller/user-blog.controller.ts
+++ apps/hs-blog-server/src/modules/user/controller/user-blog.controller.ts
@@ -1,22 +1,22 @@
-import {Body, Controller, Get, Put} from '@nestjs/common';
-import {ApiBearerAuth, ApiOperation, ApiResponse, ApiTags,} from '@nestjs/swagger';
-import {ApiResponseObject} from '@/common/decorators/api-response.decorator';
-import {userVo} from '@/modules/user/vo/user.vo';
-import {TransformToVo} from '@/common/decorators/transform-to-vo.decorator';
-import {CurrentUser} from '@/modules/auth/decorators/current-user.decorator';
-import {User} from '@/modules/user/entities/user.entity';
-import {UpdateUserDto} from '@/modules/user/dto/update-user.dto';
-import {ProfileService} from '@/modules/user/service/profile.service';
-import {UserService} from '@/modules/user/service/user.service';
-import {Public} from "@/modules/auth/decorators/public.decorator";
+import { Body, Controller, Get, Put } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { ApiResponseObject } from '@/common/decorators/api-response.decorator';
+import { userVo } from '@/modules/user/vo/user.vo';
+import { TransformToVo } from '@/common/decorators/transform-to-vo.decorator';
+import { CurrentUser } from '@/modules/auth/decorators/current-user.decorator';
+import { User } from '@/modules/user/entities/user.entity';
+import { UpdateUserDto } from '@/modules/user/dto/update-user.dto';
+import { ProfileService } from '@/modules/user/service/profile.service';
+import { UserService } from '@/modules/user/service/user.service';
+import { Public } from '@/modules/auth/decorators/public.decorator';
 
-/**
- * 用户控制器
- * 处理用户相关的 HTTP 请求
- */
 @ApiTags('blog', '用户')
 @Controller('blog/user')
-@Public()
 @ApiBearerAuth()
 export class UserBlogController {
   constructor(
@@ -36,15 +36,15 @@ export class UserBlogController {
     };
   }
 
-  @Get('stats')
-  @ApiOperation({ summary: '获取用户统计信息' })
-  async getUserStats(@CurrentUser() user: User) {
-    const stats = await this.userService.getUserStats(user.id);
-    return {
-      message: '获取用户统计信息成功',
-      data: stats,
-    };
-  }
+  // @Get('stats')
+  // @ApiOperation({ summary: '获取用户统计信息' })
+  // async getUserStats(@CurrentUser() user: User) {
+  //   const stats = await this.userService.getUserStats(user.id);
+  //   return {
+  //     message: '获取用户统计信息成功',
+  //     data: stats,
+  //   };
+  // }
 
   @Put('profile')
   @ApiOperation({ summary: '更新用户个人资料' })
@@ -61,30 +61,33 @@ export class UserBlogController {
   }
 
   @ApiOperation({ summary: '获取个人简介信息' })
+  @Public()
   @Get('personal-profile-info')
   async getConcise() {
     const result = await this.profileService.getProfile();
     return {
       data: result,
-    }
+    };
   }
 
   /**
    * 个人模块信息
    */
   @Get()
+  @Public()
   async getUserInfo() {
     const data = {
       name: 'volcano',
       avatar: '/img/avatar.jpg',
-      avatarBackgroundImage: 'https://hs-blog.oss-cn-beijing.aliyuncs.com/user/PixPin_2024-10-18_10-32-30.png',
+      avatarBackgroundImage:
+        'https://hs-blog.oss-cn-beijing.aliyuncs.com/user/PixPin_2024-10-18_10-32-30.png',
       description: '“风很温柔 花很浪漫 你很特别 我很喜欢”',
       articlesTotal: 0,
       categoriesTotal: 0,
       tagTotal: 0,
     };
     return {
-      data
-    }
+      data,
+    };
   }
 }
