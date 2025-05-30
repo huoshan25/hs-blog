6d2a83d91e96b8a10481e3168f130bd08a62f935
huoshan <1726941245@qq.com>
2025-05-30 17:33:03 +0800

    feat(server): 删除多余控制层

--- apps/hs-blog-server/src/modules/user/controller/user-admin.controller.ts
+++ apps/hs-blog-server/src/modules/user/controller/user-admin.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Put } from '@nestjs/common';
+import {Body, Controller, Delete, Get, Post, Put} from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ProfileService } from '@/modules/user/service/profile.service';
 import { UpdateProfileDto } from '@/modules/user/dto/profile.dto';
@@ -8,6 +8,9 @@ import { TransformToVo } from '@/common/decorators/transform-to-vo.decorator';
 import { CurrentUser } from '@/modules/auth/decorators/current-user.decorator';
 import { User } from '@/modules/user/entities/user.entity';
 import { UserService } from '@/modules/user/service/user.service';
+import {UserBioService} from "@/modules/user/service/user-bio.service";
+import {CreateUserBioDto, UpdateUserBioDto} from "@/modules/user/dto/user-bio.dto";
+import {UserBioVo} from "@/modules/user/vo/user-bio.vo";
 
 @ApiTags('admin', '用户模块')
 @ApiBearerAuth()
@@ -16,6 +19,7 @@ export class UserAdminController {
   constructor(
     private readonly profileService: ProfileService,
     private readonly userService: UserService,
+    private readonly userBioService: UserBioService
   ) {}
 
   @ApiOperation({ summary: '获取个人介绍' })
@@ -40,6 +44,48 @@ export class UserAdminController {
     return {
       message: '获取成功',
       data: result,
+    };
+  }
+
+  @ApiOperation({ summary: '创建或更新用户卡片信息' })
+  @Post('bio')
+  async createOrUpdate(@Body() createUserBioDto: CreateUserBioDto) {
+    await this.userBioService.createOrUpdate(createUserBioDto);
+    return {
+      message: '操作成功',
+    };
+  }
+
+  @ApiOperation({ summary: '获取用户卡片信息' })
+  @Get('bio')
+  @ApiResponseObject(UserBioVo)
+  @TransformToVo(UserBioVo)
+  async getBio() {
+    const result = await this.userBioService.getBio();
+    return {
+      message: '获取成功',
+      data: result,
+    };
+  }
+
+  @ApiOperation({ summary: '更新用户卡片信息' })
+  @Put('bio')
+  @ApiResponseObject(UserBioVo)
+  @TransformToVo(UserBioVo)
+  async update(@Body() updateUserBioDto: UpdateUserBioDto) {
+    const result = await this.userBioService.update(updateUserBioDto);
+    return {
+      message: '更新成功',
+      data: result,
+    };
+  }
+
+  @ApiOperation({ summary: '删除用户卡片信息' })
+  @Delete('bio')
+  async remove() {
+    await this.userBioService.remove();
+    return {
+      message: '删除成功',
     };
   }
 }
--- apps/hs-blog-server/src/modules/user/user.module.ts
+++ apps/hs-blog-server/src/modules/user/user.module.ts
@@ -10,10 +10,9 @@ import { ProfileService } from '@/modules/user/service/profile.service';
 import { UserService } from '@/modules/user/service/user.service';
 import { Profile } from '@/modules/user/entities/profile.entity';
 import { UserAdminController } from '@/modules/user/controller/user-admin.controller';
-import { UserConfigService } from "@/modules/user/service/user-config.service";
+import { UserConfigService } from '@/modules/user/service/user-config.service';
 import { UserBio } from '@/modules/user/entities/user-bio.entity';
 import { UserBioService } from '@/modules/user/service/user-bio.service';
-import { UserBioAdminController } from '@/modules/user/controller/user-bio-admin.controller';
 import { OssModule } from '@/modules/oss/oss.module';
 
 @Module({
@@ -27,6 +26,6 @@ import { OssModule } from '@/modules/oss/oss.module';
   ],
   providers: [UserService, ProfileService, UserConfigService, UserBioService],
   exports: [UserService, UserBioService],
-  controllers: [UserBlogController, UserAdminController, UserBioAdminController],
+  controllers: [UserBlogController, UserAdminController],
 })
 export class UserModule {}
